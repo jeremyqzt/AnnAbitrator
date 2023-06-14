edd9f87cce7c0c99c2e0f027eb1b0ba0feb67425
Jeremy Qian <jq357@hotmail.com>
2023-06-13 20:52:28 -0400

    add one more

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ from sklearn.preprocessing import MinMaxScaler, LabelBinarizer
 from sklearn.feature_extraction.text import CountVectorizer
 
 types = {"Description": "str", "Input": "str", "Status": 'int', "Type": 'int', "Amount": "float",
-         "ShipAmount": "float", "TaxAmount": "float", "TotalAmount": "float", "Success": "int"}
+         "ShipAmount": "float", "TaxAmount": "float", "TotalAmount": "float", "Success": "int", "SKU": "str"}
 csv_train = pd.read_csv(
     "./data/amount.csv",
-    names=["Description", "Input", "Status", "Type", "Amount", "ShipAmount", "TaxAmount",
+    names=["Description", "Input", "SKU", "Status", "Type", "Amount", "ShipAmount", "TaxAmount",
            "TotalAmount", "Success"],
     encoding="utf-8",
     dtype=types, keep_default_na=False)
@@ -29,14 +29,15 @@ in_vec = csv_train[['Status', 'Type', 'Amount',
 
 to_train = []
 for vec in in_vec:
-    to_convert = make_input_vector([{"size": 7, "to_set": vec[0]}, {
-        "size": 50, "to_set": vec[1]}, {"size": 1, "to_set": vec[3]}, {"size": 1, "to_set": vec[4]},
+    to_convert = make_input_vector([
+        {"size": 50, "to_set": vec[1]}, {"size": 1, "to_set": vec[3]}, {"size": 1, "to_set": vec[4]},
         {"size": 1, "to_set": vec[5]}])
     to_train.append(to_convert)
 
 csv_train.to_csv(
     'data/des.txt', columns=["Description"], index=False, header=False)
 csv_train.to_csv('data/inp.txt', columns=["Input"], index=False, header=False)
+csv_train.to_csv('data/sku.txt', columns=["SKU"], index=False, header=False)
 
 expected = csv_train[['Success']]
 create_model = True
@@ -48,16 +49,18 @@ all_tokens = trainingDataIn + trainingDataOut
 tokenizer = get_tokenizer(all_tokens)
 trainingDataIn = tokenize(tokenizer, load_doc("data/des.txt").rstrip())
 trainingDataOut = tokenize(tokenizer, load_doc("data/inp.txt").rstrip())
+trainingDataSKU = tokenize(tokenizer, load_doc("data/sku.txt").rstrip())
 
 n_words = trainingDataIn.shape[1]
 n_words2 = trainingDataOut.shape[1]
+n_words3 = trainingDataSKU.shape[1]
 
 pos = create_mlp(n_words)
 neg = create_mlp(n_words2)
-
+sku = create_category_mlp(n_words3)
 other = create_category_mlp(len(to_train[0]))
 
-combinedInput = concatenate([pos.output, neg.output, other.output])
+combinedInput = concatenate([pos.output, neg.output, other.output, sku.output])
 
 x = Dense(16, activation="relu")(combinedInput)
 x = Dense(8, activation="relu")(x)
@@ -65,7 +68,7 @@ x = Dense(1, activation="linear")(x)
 
 if create_model:
 
-    model = Model(inputs=[pos.input, neg.input, other.input], outputs=x)
+    model = Model(inputs=[pos.input, neg.input, other.input, sku.input], outputs=x)
     model.compile(loss='binary_crossentropy',
                   optimizer='adam', metrics=['accuracy'])
 
